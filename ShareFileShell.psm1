81e47a7c303fb1d07ec0ba37c2ebd2e0adc37a10
briped <briped@live.com>
2024-07-09 10:15:30 +0200

    Fixed/updated Remove-Client

--- ShareFileShell.psm1
+++ ShareFileShell.psm1
@@ -1152,7 +1152,7 @@ function Remove-Client {
 				,  ValueFromPipeline = $true
 				,  ValueFromPipelineByPropertyName = $true)]
 		[ValidateNotNullOrEmpty()]
-		[string]
+		[string[]]
 		$Id
 		,
 		# Force remove. Don't ask for confirmation unless explicitly specified.
@@ -1160,34 +1160,44 @@ function Remove-Client {
 		[switch]
 		$Force
 	)
-	$Script:Token = Get-Token
-	if ($Force -and !$Confirm) {
-		$ConfirmPreference = 'None'
+	begin {
+		$Script:Token = Get-Token
+		if ($Force -and !$Confirm) {
+			$ConfirmPreference = 'None'
+		}
+		$Header = @{
+			Authorization = "Bearer $($Script:Token.access_token)"
+		}
 	}
-	$Header = @{
-		Authorization = "Bearer $($Script:Token.access_token)"
-	}
-	$Data = @{
-		UserIds = @($Id)
-	}
-	$Splatter = @{
-		ContentType = 'application/json'
-		#Method = 'DELETE'
-		#Uri = "https://$($Script:Token.subdomain).$($Script:Token.apicp)/sf/v3/Users/Clients"
-		Method = 'POST'
-		Uri = "https://$($Script:Token.subdomain).$($Script:Token.apicp)/sf/v3/Users/Clients/BulkDelete"
-		Header = $Header
-		Body = $Data | ConvertTo-Json -Compress
-	}
-	if ($Script:Config.Proxy) {
-		$Splatter.Proxy = $Script:Config.Proxy
-	}
-
-	$SfUser = Get-User -Id $Id
-	if ($PSCmdlet.ShouldProcess("'$($SfUser.FullName)' ($($SfUser.Username)) ($($SfUser.Id)).")) {
-		Write-Verbose -Message "$($MyInvocation.MyCommand.Name) : Deleting user: '$($SfUser.FullName)' ($($SfUser.Username)) ($($SfUser.Id))."
-		Write-Verbose -Message "$($MyInvocation.MyCommand.Name) : Invoke-RestMethod @$($Splatter | ConvertTo-Json -Compress)"
-		Invoke-RestMethod @Splatter
+	process {
+		if ($id.Count -gt 1) {
+			$UserIds = $Id
+			$Message = "Removing $($Id.Count) clients."
+		}
+		else {
+			$SfUser = Get-User -Id $Id[0]
+			$UserIds = @($SfUser.Id)
+			$Message = "Removing 'Client': '$($SfUser.FullName)' ($($SfUser.Username)) ($($SfUser.Id))."
+		}
+		$Splatter = @{
+			ContentType = 'application/json'
+			Header = $Header
+			Method = 'DELETE'
+			Uri = "https://$($Script:Token.subdomain).$($Script:Token.apicp)/sf/v3/Users/Clients"
+			Body = $UserIds | ConvertTo-Json -AsArray -Compress
+			#Method = 'POST'
+			#Uri = "https://$($Script:Token.subdomain).$($Script:Token.apicp)/sf/v3/Users/Clients/BulkDelete"
+			#Body = @{UserIds = $UserIds} | ConvertTo-Json -Compress
+		}
+		if ($Script:Config.Proxy) {
+			$Splatter.Proxy = $Script:Config.Proxy
+		}
+	
+		if ($PSCmdlet.ShouldProcess($Message)) {
+			Write-Verbose -Message "$($MyInvocation.MyCommand.Name) : $($Message)"
+			Write-Verbose -Message "$($MyInvocation.MyCommand.Name) : Invoke-RestMethod @$($Splatter | ConvertTo-Json -Compress)"
+			Invoke-RestMethod @Splatter
+		}
 	}
 }
 function Revoke-Employee {
